29f9011c252ca8f07245e12af2d1c279bbc15885
PomesHouse <102507648+PomesHouse@users.noreply.github.com>
2022-09-08 15:50:41 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,10 +4,51 @@ const button3 = document.querySelector('.button_3')
 
 
 const table1 = document.querySelector('.table1')
+const table2 = document.querySelector('.table2')
+const table3 = document.querySelector('.table3')
+const table_off = document.querySelector('.table_off')
+
+
 
 function open_table1(){
-    table1.style.display = 'flex';}
+    table2.style.display = 'none';
+    table3.style.display = 'none';
+    if (table1.style.display == 'none') {
+        table1.style.display = 'flex';
+        table_off.style.display = 'none';
+    } else {table1.style.display = 'none';
+           table_off.style.display = 'inline-block'}
+    }
+    
+
+function open_table2(){
+    table1.style.display = 'none';
+    table3.style.display = 'none';
+    if (table2.style.display == 'none') {
+        table2.style.display = 'flex';
+        table_off.style.display = 'none';
+    } else {table2.style.display = 'none';
+           table_off.style.display = 'inline-block'}
+    }
+    
+function open_table3(){
+    table1.style.display = 'none';
+    table2.style.display = 'none';
+    if (table3.style.display == 'none') {
+        table3.style.display = 'flex';
+        table_off.style.display = 'none';
+    } else {table3.style.display = 'none';
+           table_off.style.display = 'inline-block'}
+    }
+    
+ 
+
+function close_tables(){
+    tables.style.display = 'none';
+}
 
 
 
-button1.addEventListener('click', open_table1)
+button1.addEventListener('click', open_table1 );
+button2.addEventListener('click', open_table2 ); 
+button3.addEventListener('click', open_table3 );
