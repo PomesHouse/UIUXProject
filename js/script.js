72aa6c666d02677ebffadf65e5e0b34b19005600
PomesHouse <102507648+PomesHouse@users.noreply.github.com>
2022-09-14 10:15:36 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ const button1 = document.querySelector('.button_1')
 const button2 = document.querySelector('.button_2')
 const button3 = document.querySelector('.button_3')
 
+const button1Img = document.querySelector('#button_img1')
+const button2Img = document.querySelector('#button_img2')
+const button3Img = document.querySelector('#button_img3')
+
 
 const table1 = document.querySelector('.table1')
 const table2 = document.querySelector('.table2')
@@ -13,32 +17,44 @@ const table_off = document.querySelector('.table_off')
 function open_table1(){
     table2.style.display = 'none';
     table3.style.display = 'none';
+    button2Img.style.display = 'none';
+    button3Img.style.display = 'none';
     if (table1.style.display == 'none') {
         table1.style.display = 'flex';
         table_off.style.display = 'none';
+        button1Img.style.display = 'inline-block';
     } else {table1.style.display = 'none';
-           table_off.style.display = 'inline-block'}
+           table_off.style.display = 'inline-block';
+           button1Img.style.display = 'none';}
     }
     
 
 function open_table2(){
     table1.style.display = 'none';
     table3.style.display = 'none';
+    button1Img.style.display = 'none';
+    button3Img.style.display = 'none';
     if (table2.style.display == 'none') {
         table2.style.display = 'flex';
         table_off.style.display = 'none';
+        button2Img.style.display = 'inline-block';
     } else {table2.style.display = 'none';
-           table_off.style.display = 'inline-block'}
+           table_off.style.display = 'inline-block';
+           button2Img.style.display = 'none';}
     }
     
 function open_table3(){
     table1.style.display = 'none';
     table2.style.display = 'none';
+    button1Img.style.display = 'none';
+    button2Img.style.display = 'none';
     if (table3.style.display == 'none') {
         table3.style.display = 'flex';
         table_off.style.display = 'none';
+        button3Img.style.display = 'inline-block';
     } else {table3.style.display = 'none';
-           table_off.style.display = 'inline-block'}
+           table_off.style.display = 'inline-block';
+           button3Img.style.display = 'none';}
     }
     
  
